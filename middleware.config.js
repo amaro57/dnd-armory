b1c78d312ec9c2eee191299ec00b7f20fafa7ebf
amaro57 <abushnaqdev@outlook.com>
2022-02-10 21:33:43 -0600

    Remove key

--- middleware.config.js
+++ middleware.config.js
@@ -5,7 +5,7 @@ module.exports = {
       configuration: {
         api: {
           domain: 'dnd-armory.myshopify.com',
-          storefrontAccessToken: 'd223f01c0d01c96fcf7379342ffb88ac'
+          storefrontAccessToken: ''
         },
         currency: 'USD',
         country: 'US'
